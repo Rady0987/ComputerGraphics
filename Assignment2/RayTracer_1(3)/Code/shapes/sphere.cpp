2dd64a36ac442969e8e521d3213ea23e7998a2d4
naut <naut.folkers@gmail.com>
2022-03-03 12:34:21 +0100

    4.1 first try

--- Assignment2/RayTracer_1(3)/Code/shapes/sphere.cpp
+++ Assignment2/RayTracer_1(3)/Code/shapes/sphere.cpp
@@ -29,6 +29,28 @@ Hit Sphere::intersect(Ray const &ray)
 
     double t = 1000;
 
+
+    /**************** OUR CODE *************************/
+
+    double A = (ray.D).dot(ray.D); // d*d
+    double B = (ray.D).dot(ray.O-position); // 2d * (e - c)
+    double C = (ray.O-position).dot(ray.O-position) - (r*r); // (e - c) * (e - c) - R*R
+
+    double D = sqrt((B*B) - (A*C))
+
+    if (D < 0) {
+        return Hit::NO_HIT();
+    } elseif (D == 0) { // there is only one point in which the ray intersect the sphere
+        double t = -B/A;
+        return Hit(t, N);
+    } else { // there are two points and the smallest t is the one closest to the eye/camera
+        double t1 = (-B + D)/A;
+        double t2 = (-B - D)/A;
+        t = (t1 < t2 ? t1 : t2);
+        return Hit(t, N);
+    }
+
+
     /****************************************************
     * RT1.2: NORMAL CALCULATION
     *
